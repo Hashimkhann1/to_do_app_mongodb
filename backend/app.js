80cd033a27c3570889674e68d079c4cdaeb4040b
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-14 23:07:35 +0500

    express is defined and its main functionlaty is writed in this fil

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRouter = require('./routers/user_routes');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use("/",userRouter)
+
+module.exports = app;
