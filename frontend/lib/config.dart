8e5cc2d96a39eb057ebfc485ae466ae97c785d68
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-15 23:29:08 +0500

    endpoints of api

---
+++ frontend/lib/config.dart
@@ -0,0 +1,6 @@
+final url = 'http://192.168.29.239:3000/';
+final registration = url + "registration";
+final login = url + 'login';
+final addtodo = url + 'storeTodo';
+final getToDoList = url + 'getUserTodoList';
+final deleteTodo = url + 'deleteTodo';
