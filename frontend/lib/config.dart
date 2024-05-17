cf9b9655dcd8b12180378ec44c7249d857e9c96b
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-17 23:49:29 +0500

    base url is changed

--- frontend/lib/config.dart
+++ frontend/lib/config.dart
@@ -1,4 +1,4 @@
-final url = 'http://192.168.29.239:3000/';
+final url = 'http://10.0.2.2:5000/';
 final registration = url + "registration";
 final login = url + 'login';
 final addtodo = url + 'storeTodo';
