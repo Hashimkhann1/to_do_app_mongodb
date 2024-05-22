dcecab7156d6e89b67a25856642739d787e41f76
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-22 23:44:48 +0500

    login route is added

--- backend/routers/user_routes.js
+++ backend/routers/user_routes.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const userController = require('../controller/user_controller');
 
 router.post('/registration',userController.register);
+router.post('/login',userController.login);
+
 
 module.exports = router
