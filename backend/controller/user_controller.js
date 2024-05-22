67c2100b5c23d57e82607cf8b476e6c04968302f
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-22 23:45:35 +0500

    login is completed

--- backend/controller/user_controller.js
+++ backend/controller/user_controller.js
@@ -1,4 +1,5 @@
 const UserServices = require('../services/user_services');
+const UserModel = require('../model/user_model')
 
 exports.register = async (req , res , next) => {
     try{
@@ -8,5 +9,32 @@ exports.register = async (req , res , next) => {
         res.json({status: true,success: "User register Successfully"});
     }catch(err){
         throw err;
+    }
+}
+
+exports.login = async (req , res , next) => {
+    try{
+        const {email , password} = req.body;
+        
+        const user = await UserServices.checkUser(email);
+
+        if(!user){
+            throw new Error("User note exist!");
+        }
+
+        const isMatch = await user.comparePassword(password);
+        if(isMatch === false){
+            throw new Error("Password is Invalid");
+        }
+
+        let tokenData = {_id:user._id,email:user.email};
+        const token = await UserServices.generateToken(tokenData , "secretKey" , "1h");
+
+        res.status(200).json({status:true,token:token});
+
+    }catch(err){
+        console.log('error' + err);
+        throw err;
+        
     }
 }
