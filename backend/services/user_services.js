e933d7e948cb2186188d2c2b5eddcde11e27a856
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-22 23:44:24 +0500

    login is completed

--- backend/services/user_services.js
+++ backend/services/user_services.js
@@ -1,7 +1,9 @@
 const UserModel = require('../model/user_model');
+const jwt = require('jsonwebtoken');
 
 class UserServices {
 
+    /// register user method
     static async registerUser(email , password) {
         try{
             const createUser = new UserModel({email,password});
@@ -9,6 +11,19 @@ class UserServices {
         }catch(error){
             throw error;
         }
+    }
+
+    /// login method but checking thata user exist or not with enter email
+    static async checkUser(email) {
+        try{
+            return await UserModel.findOne({email});
+        }catch(err){
+            throw err;
+        }
+    }
+
+    static async generateToken(tokenData , secretKey , jwt_expire) {
+        return jwt.sign(tokenData , secretKey , {expiresIn: jwt_expire})
     }
 
 }
