c2c796ad41a86c583d6870ff20598c40e4239fe1
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-27 23:23:08 +0500

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 const db = require('./config/db')
 const userModel = require('./model/user_model');
+const TodoModel = require('./model/todo_model');
 
 app.get('/',(req , res) => {
     res.send("Hi, server is connected :)");
