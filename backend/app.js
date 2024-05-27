be5daec67d84bee1ac6e39f43ebff2ef5fae8274
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-27 23:22:48 +0500

    updated

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRouter = require('./routers/user_routes');
+const todoRouter = require('./routers/todo_routes');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use("/",userRouter)
+app.use("/",userRouter);
+app.use("/",todoRouter);
 
 module.exports = app;
